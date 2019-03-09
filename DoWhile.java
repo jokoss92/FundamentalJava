1d4489cdb3c8aea4b726c896dbea59c9853e071f
Joko Sarjono S <johno.smakaduta@gmail.com>
2019-03-09 23:35:57 +0700

    Add Switch Case

---
+++ DoWhile.java
@@ -0,0 +1,9 @@
+public class DoWhile{
+    public static void main(String[] args){
+    int x = 0; //1
+    do { //2
+        System.out.println(x); //3
+        x++; //4 bacl to 1
+    } while(x < 5);
+    }
+}
---
+++ WhileLoop.java
@@ -0,0 +1,9 @@
+public class WhileLoop{
+    public static void main(String[] args){
+    int x = 0; //1
+    while (x < 10){ //2
+        System.out.println(x); //3
+        x++; //4 bacl to 1
+    }
+    }
+}
